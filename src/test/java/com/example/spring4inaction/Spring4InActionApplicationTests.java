97ef391078f2b2915f20f7954c37b46e71ae4340
yangwuse <yangwuse@mail.ustc.edu.cn>
2021-04-14 21:16:51 +0800

    ok

---
+++ src/test/java/com/example/spring4inaction/Spring4InActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring4inaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring4InActionApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
